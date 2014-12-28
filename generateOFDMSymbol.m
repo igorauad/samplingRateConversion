d027c000feb69f8d0e281ce0a35323ef3a187cfa
Igor Freire <igor.auad@gmail.com>
2014-12-27 21:50:08 -0300

    Add comments to function for random OFDM symbol generation

--- generateOFDMSymbol.m
+++ generateOFDMSymbol.m
@@ -1,19 +1,46 @@
-function [ x_serial ] = generateOFDMSymbol( Nfft, nSymbols, fl, fup )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ x_serial ] = generateOFDMSymbol(Nfft, nSymbols, f_lo, f_hi, fs)
+% Generate a random Hermitian Symmetric OFDM symbol
+% ------------------------------------------------
+%   [ x_serial ] = generateOFDMSymbol( Nfft, nSymbols, fl, fup )
+%
+%   
+%   Inputs:
+%       Nfft        -   FFT Size
+%       nSymbols    -   Number of Symbols to Generate
+%       f_lo        -   Lowest normalized frequency to be used 
+%       f_hi        -   Highest normalized frequency to be used
+%
+%   Output:
+%       x_serial    -   Serialized time domain OFDM symbols
+
 
 M = 16;         % QAM constellation
+
+% Check if frequencies are valid:
+if (f_lo > f_hi)
+   error('Lower freq. can not be higher the Highest freq.'); 
+end
+
+if (f_lo > (fs/2) || f_hi > (fs/2))
+   error('Frequency higher can not be higher than Nyquist.'); 
+end
+
+% Normalize Upper and Lower bounds:
+f_lo = f_lo / (fs/2);
+f_hi = f_hi / (fs/2);
+% Note: this converts the frequencies to the range (0, 1), where 0 means DC
+% and 1 means Nyquist.
 
 % OFDM Modulation
 
 % Modulator Object:
-mod     =   modem.qammod('M', M, 'SymbolOrder', 'Gray');
+mod = modem.qammod('M', M, 'SymbolOrder', 'Gray');
 
 % Preallocate Positive side of the spectrum
 X_pos = zeros(Nfft/2 +1, nSymbols);
 
 % Use tones within a limited bandwidth:
-usedTones = floor(fl*(Nfft/2)) : floor(fup*(Nfft/2));
+usedTones = (floor(f_lo*(Nfft/2 + 1)) + 1) : floor(f_hi*(Nfft/2 + 1));
 
 % Modulate:
 X_pos(usedTones, :) = mod.modulate(randi(M, length(usedTones), nSymbols) - 1);
@@ -26,7 +53,7 @@ X_hermitian(1) = abs(X_hermitian(1));
 X_hermitian(Nfft/2 + 1) = abs(X_hermitian(Nfft/2 + 1));
 
 % Ifft:
-x = ifft(X_hermitian);
+x = ifft(X_hermitian, Nfft);
 
 % P/S conversion:
 x_serial = x(:);
